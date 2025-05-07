4fb11d50f5b6ec53120536ea4d4a7d62280ed5d1
tomas.koleckar <tomas.koleckar@ecmsystem.cz>
2025-05-07 10:34:51 +0200

    getting dashboards from nodes

--- src/components/ecm-marfy-components/marfy-layout/aside-marfy-component.vue
+++ src/components/ecm-marfy-components/marfy-layout/aside-marfy-component.vue
@@ -102,7 +102,7 @@
     async function handleNodeClick(node: any) {
         store.selectedNodeId = node.id;
         console.log(store.selectedNodeId);
-        let dashboards = await axios.get(`${import.meta.env.VITE_API_URL}Dashboard`, {
+        let dashboards = await axios.get(`${import.meta.env.VITE_API_URL}Dashboard/GetDashboards`, {
             params: {
                 nodeId: node.id,
                 orgId: store.selectedOrgId
