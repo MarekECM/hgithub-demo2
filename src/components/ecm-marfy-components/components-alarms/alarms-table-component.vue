3a4ad0b8fb97b865f15b18942313ecfb65ac6d29
Marek Valalík <marek.valalik@ecmsystem.cz>
2025-05-16 12:54:29 +0200

    alarmy

--- src/components/ecm-marfy-components/components-alarms/alarms-table-component.vue
+++ src/components/ecm-marfy-components/components-alarms/alarms-table-component.vue
@@ -28,7 +28,10 @@
                 <span class="alarm-detail__status alarm-detail__status--ok">ok</span>
             </td>
             <td class="alarm-detail__cell alarm-detail__cell--actions">
-                 <span class="alarm-detail__icon material-icons" style="font-size: 25px;">schedule</span>
+                <span class="alarm-detail__icon material-icons" style="font-size: 25px;">schedule</span>
+                <span class="alarm-detail__icon material-icons" style="font-size: 25px;">edit</span>
+                <span class="alarm-detail__icon material-icons" style="font-size: 25px;">check_circle</span>
+                <span class="alarm-detail__icon material-icons" style="font-size: 25px;">delete</span>
             </td>
         </tr>
         <tr class="alarm-detail__row">
